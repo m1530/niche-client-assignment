6656746004dd80d150f5bdbfcc5cdd7b6441455e
m1530 <rahmanmoshiur253@gmail.com>
2021-11-13 17:18:59 +0600

    add dashboardhghjgh

--- src/components/Login/Firebase/firebase.config.js
+++ src/components/Login/Firebase/firebase.config.js
@@ -7,10 +7,3 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
 };
 export default firebaseConfig;
-
-// apiKey: "AIzaSyA2tHMNsY1JnmQbr5EzmA4nhjkPxxm5jFk",
-// authDomain: "car-website-5710a.firebaseapp.com",
-// projectId: "car-website-5710a",
-// storageBucket: "car-website-5710a.appspot.com",
-// messagingSenderId: "1393078974",
-// appId: "1:1393078974:web:c434ca46882b4f2d7024ef"
